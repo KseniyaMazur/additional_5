c6481eabeaa19d537659b93968e827e130070bec
KseniyaMazur <ksuha80018@gmail.com>
2018-02-27 23:14:20 +0300

    brackets/feature: impplement function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  const lengthStr = str.length;
+  const lengthBracketsConfig = bracketsConfig.length;
+  let bracketsStack = [];
+
+  for (let i = 0; i < lengthStr; i++){
+    element = str[i];
+    for (let j = 0; j < lengthBracketsConfig; j++) {
+      if (element === bracketsConfig[j][0] && bracketsConfig[j][0] === bracketsConfig[j][1]){
+        if (element === bracketsStack[bracketsStack.length-1]){
+          bracketsStack.pop();
+        } else {
+          bracketsStack.push(element);
+        }
+      } else if (element === bracketsConfig[j][0]){
+        bracketsStack.push(element);
+      } else if (element === bracketsConfig[j][1]){
+        if (bracketsStack.length !== 0 && (bracketsStack.pop() === bracketsConfig[j][0])){
+          continue;
+        } else {
+          return false;
+        };
+      };
+    };
+  };
+  if (bracketsStack.length != 0) {
+    return false;
+  };
+  return true;
 }
